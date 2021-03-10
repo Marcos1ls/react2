101175f882cd9111b728b88c91a0aa2dde1136c8
Marcos <mlijos16@alumnes.santjosepobrer.es>
2021-03-10 18:59:33 +0100

    Ejercicio 5

--- react2/pages/ejercicio.js
+++ react2/pages/ejercicio.js
@@ -1,49 +1,45 @@
 import { useState } from 'react'
 
 const ProgramaListadoAlumnos = () => {
+    const listaDatos = ["Ana", "Bernat", "Carol", "David", "Elena", "Francesc"]
+    const [nombre, setNombre] = useState('')
+    const [listaAlumnos, setListaAlumnos] = useState(listaDatos)
+    const empiezaLetra = (cadena) => /^[a-zA-Z]/.test(cadena)
+    const noestaVacio = (cadena) => cadena.trim() != 0
 
-  const listaDatos = ["Ana","Bernat","Carol","David","Elena","Francesc"]
-  const [nombre, setNombre] = useState('')
-  const [listaAlumnos, setListaAlumnos] = useState(listaDatos)
-  const empiezaLetra = (cadena) => /^[a-zA-Z]/.test(cadena)
-  const noestaVacio = (cadena) => cadena.trim() !=0
-
-  const mostrar = (lista) => {
-    const lista_formato = lista.map((elemento, index) => {
-      return <li>{index} - <b>{elemento}</b></li>
-    })
-    return lista_formato
-  }
-
-  const agregarAlumno = e => {
-    e.preventDefault()
-    if (noestaVacio(nombre) && empiezaLetra(nombre)){
-      setListaAlumnos([
+    const mostrar = (lista) => {
+      const lista_formato = lista.map((elemento, index) => {
+        return <li> {index} - <b> {elemento} </b></li>
+      })
+      return lista_formato
+    }
+    const agregarAlumno = e => {
+      e.preventDefault()
+      const validar = noestaVacio(nombre) && empiezaLetra(nombre)
+      
+      validar && setListaAlumnos([
         ...listaAlumnos, nombre
       ])
-      setNombre('')
+      
+      validar && setNombre('')
+      !validar && alert('Dato no válido')
     }
-    else {
-      alert('Dato no válido')
-    }
-  }
-
-  return (
+    return (
     <div>
       <h2>Lista de Alumnos</h2>
       <ul>
         {mostrar(listaAlumnos)}
       </ul>
-      <form onSubmit={agregarAlumno}>
+      <form onSubmit = {agregarAlumno}>
         <input
         type="text"
         placeholder="Introduce nuevo alumno"
-        onChange={e => setNombre(e.target.value)}
-        value={nombre}
+        onChange = {e => setNombre(e.target.value)}
+        value = {nombre}
+        minLength="2"
         maxLength="10"
         />
       </form>
     </div>
-  )
-}
+  )}
 export default ProgramaListadoAlumnos
